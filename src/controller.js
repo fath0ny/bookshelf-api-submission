b0909e7823129a4f884f52b86d8e5d403520fb8a
fath0ny <arief.fathony10@gmail.com>
2026-09-24 15:47:53 +0700

    Adding updateBookById for Kriteria 6 : API dapat mengubah data buku

--- src/controller.js
+++ src/controller.js
@@ -71,3 +71,37 @@ export const getBookById = (req, res) => {
     message: "Buku tidak ditemukan"
   });
 };
+
+export const updateBookById = (req, res) => {
+  const { id } = req.params;
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = req.body;
+  const updatedAt = new Date().toISOString();
+  const index = book.findIndex((b) => b.id === id);
+
+  if (index !== -1) {
+    book[index] = {...book[index], name, year, author, summary, publisher, pageCount, readPage, reading, updatedAt};
+    if (!name) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Gagal memperbarui buku. Mohon isi nama buku"
+      });
+    }
+
+    if (readPage > pageCount) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount"
+      });
+    }
+    
+    return res.status(200).json({
+      status: "success",
+      message: "Buku berhasil diperbarui"
+    });
+  }
+
+  return res.status(404).json({
+    status: "fail",
+    message: "Gagal memperbarui buku. Id tidak ditemukan"
+  });
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createBook, getBook, getBookById } from './controller.js';
+import { createBook, getBook, getBookById, updateBookById } from './controller.js';
 
 const router = express.Router();
 router.post('/books', createBook);
 router.get('/books', getBook);
 router.get('/books/:id', getBookById)
+router.put('/books/:id', updateBookById);
 
 export default router;
